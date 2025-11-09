e32ea8ee45949a424b46372cd789e181aab96242
Asım <asimblsmc16@gmail.com>
2025-11-09 13:39:39 +0300

    Add files via upload

---
+++ dStokVeritabanı/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnekle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnsil = new System.Windows.Forms.Button();
+            this.btnguncelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(284, 36);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(272, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnekle
+            // 
+            this.btnekle.Location = new System.Drawing.Point(191, 100);
+            this.btnekle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnekle.Name = "btnekle";
+            this.btnekle.Size = new System.Drawing.Size(365, 28);
+            this.btnekle.TabIndex = 1;
+            this.btnekle.Text = "EKLE";
+            this.btnekle.UseVisualStyleBackColor = true;
+            this.btnekle.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(188, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kategori Adı:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(191, 171);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(365, 185);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // btnsil
+            // 
+            this.btnsil.Location = new System.Drawing.Point(191, 132);
+            this.btnsil.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnsil.Name = "btnsil";
+            this.btnsil.Size = new System.Drawing.Size(365, 28);
+            this.btnsil.TabIndex = 4;
+            this.btnsil.Text = "SİL";
+            this.btnsil.UseVisualStyleBackColor = true;
+            this.btnsil.Click += new System.EventHandler(this.btnsil_Click);
+            // 
+            // btnguncelle
+            // 
+            this.btnguncelle.Location = new System.Drawing.Point(191, 68);
+            this.btnguncelle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnguncelle.Name = "btnguncelle";
+            this.btnguncelle.Size = new System.Drawing.Size(365, 28);
+            this.btnguncelle.TabIndex = 5;
+            this.btnguncelle.Text = "GÜNCELLE";
+            this.btnguncelle.UseVisualStyleBackColor = true;
+            this.btnguncelle.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnguncelle);
+            this.Controls.Add(this.btnsil);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnekle);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnekle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnsil;
+        private System.Windows.Forms.Button btnguncelle;
+    }
+}
---
+++ dStokVeritabanı/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void kategorigoster()
+        {
+            baglanti.Open();
+
+            SqlDataAdapter da = new SqlDataAdapter("select * from kategoriler", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+
+            baglanti.Close();
+        }
+
+        SqlConnection baglanti = new SqlConnection("Data Source= localhost\\SQLEXPRESS; Initial Catalog=Stok_Takip; User ID=sa; Password=Asim18");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand ekle = new SqlCommand("insert into kategoriler(kategori_adi) values(@katAd)", baglanti);
+            ekle.Parameters.AddWithValue("@katAd", textBox1.Text);
+            ekle.ExecuteNonQuery();
+            textBox1.Clear();
+            baglanti.Close();
+
+            kategorigoster();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kategorigoster();
+        }
+
+        private void btnsil_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand sil = new SqlCommand("delete from kategoriler where kategori_id=@katId;", baglanti);
+            sil.Parameters.AddWithValue("katId", dataGridView1.CurrentRow.Cells[0].Value);
+            sil.ExecuteNonQuery();
+
+            baglanti.Close();
+
+            kategorigoster();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand guncelle = new SqlCommand("update kategoriler set kategori_Adi=@katAdi where kategori_id=@katId;", baglanti);
+            guncelle.Parameters.AddWithValue("@katAdi", textBox1.Text);
+            guncelle.Parameters.AddWithValue("@katId", dataGridView1.CurrentRow.Cells[0].Value);
+            guncelle.ExecuteNonQuery();
+
+            baglanti.Close();
+
+            kategorigoster();
+
+            textBox1.Clear();
+        }
+    }
+}
